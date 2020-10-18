9d275a30d867036cdecb9b27ad3d30ea00bb2bb0
Eyoatam <eyoatamtamirat7@gmail.com>
2020-10-18 14:55:14 +0300

    updates

--- bot2.js
+++ bot2.js
@@ -3,31 +3,32 @@ const fs = require("fs");
 const Telegraf = require("telegraf");
 const Extra = require("telegraf/extra");
 const https = require("https");
+const request = require("request");
 const { sendPostRequest } = require("./botController/BotRequest");
 const telgramMessager = require("./Routes/messagesRoutes");
 
 const bot = new Telegraf("1362797784:AAGSw88xsIT-EiazAPV3WH9oOEZdCjUlq-U");
 
-bot.use(Telegraf.log());
+// bot.use(Telegraf.log());
 
-bot.command("prayertime", (ctx) => {
-	return ctx.reply(
-		"Choose your preffered prayer date",
-		Extra.HTML().markup((m) =>
-			m.inlineKeyboard([
-				m.callbackButton("MON", "1pm"),
-				m.callbackButton("TUE", "10am"),
-				m.callbackButton("THU", "3pm"),
-				m.callbackButton("FRI", "5pm"),
-				m.callbackButton("SAT", "4pm"),
-			])
-		)
-	);
-});
+// bot.command("prayertime", (ctx) => {
+// 	return ctx.reply(
+// 		"Choose your preffered prayer date",
+// 		Extra.HTML().markup((m) =>
+// 			m.inlineKeyboard([
+// 				m.callbackButton("MON", "1pm"),
+// 				m.callbackButton("TUE", "10am"),
+// 				m.callbackButton("THU", "3pm"),
+// 				m.callbackButton("FRI", "5pm"),
+// 				m.callbackButton("SAT", "4pm"),
+// 			])
+// 		)
+// 	);
+// });
 
-bot.action(/.+/, (ctx) => {
-	return ctx.reply("Good Choice");
-});
+// bot.action(/.+/, (ctx) => {
+// 	return ctx.reply("Good Choice");
+// });
 
 bot.command("start", (ctx) => {
 	return ctx.reply(
@@ -35,38 +36,52 @@ bot.command("start", (ctx) => {
 		Extra.markup((markup) => {
 			return markup
 				.resize()
-				.keyboard([
-					markup.contactRequestButton("Send contact", "contact"),
-					markup.locationRequestButton("Send location", "location"),
-				]);
+				.keyboard([markup.contactRequestButton("Send contact")]);
 		})
 	);
 });
 
 bot.on("contact", (ctx) => {
-	ctx.reply(
-		`first_name: ${ctx.update.message.chat.first_name}, last_name: ${ctx.update.message.chat.last_name}, username: ${ctx.update.message.chat.username}, chat_id: ${ctx.update.message.chat.id}, phone_number:${ctx.update.message.contact.phone_number}, user_id:${ctx.update.message.contact.user_id}`
-	);
 	let userObject = {
 		first_name: ctx.update.message.chat.first_name,
 		last_name: ctx.update.message.chat.last_name,
 		username: ctx.update.message.chat.username,
-		chat_id: ctx.update.message.chat.chat.id,
+		Chat_Id: ctx.update.message.chat.id,
 		phone_number: ctx.update.message.contact.phone_number,
 	};
-	sendPostRequest("POST", userObject, (error, response, body) => {
-		if (err) {
+	const api_url = "https://instant-prayer-api.herokuapp.com/api/users";
+	var options = {
+		uri: api_url,
+		method: "POST",
+		json: userObject,
+	};
+	request(options, function (error, response, body) {
+		if (error) {
 			console.log(error);
+		} else {
+			ctx.reply("Welcome Dear User");
 		}
-		console.log("request sent");
 	});
 });
 
-bot.on("location", (ctx) => {
-	ctx.reply(
-		`Latitde: ${ctx.update.message.location.latitude}, Longitude: ${ctx.update.message.location.longitude}`
-	);
-});
+// bot.on("location", (ctx) => {
+// 	ctx.reply(
+// 		`Latitde: ${ctx.update.message.location.latitude}, Longitude: ${ctx.update.message.location.longitude}`
+// 	);
+// 	let userObject = {
+// 		latitude: ctx.update.message.location.latitude,
+// 		longitude: ctx.update.message.location.longitude,
+// 	};
+// 	const api_url = "https://instant-prayer-api.herokuapp.com/api/users";
+// 	var options = {
+// 		uri: api_url,
+// 		method: "POST",
+// 		json: userObject,
+// 	};
+// 	request(options, function (error, response, body) {
+// 		callback(error, response, body);
+// 	});
+// });
 
 bot.launch();
 
