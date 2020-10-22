e44cd596e4b1c624975c3d22e80f40e4b44eae89
Eyoatam <eyoatamtamirat7@gmail.com>
2020-10-22 15:20:44 +0300

    updates

--- index.js
+++ index.js
@@ -130,14 +130,14 @@ bot.action(/.+/, (ctx) => {
 		method: "POST",
 		json: prefferedDate,
 	};
-	return ctx.reply(`${ctx.match[0]} is your prayer date`);
-	// request(options, function (error, response, body) {
-	// 	if (error) {
-	// 		console.log(error);
-	// 	} else {
-	// 		return ctx.reply(`${ctx.match[0]} is your prayer date`);
-	// 	}
-	// });
+	request(options, function (error, response, body) {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log(response.headers);
+			return ctx.reply(`${ctx.match[0]} is your prayer date`);
+		}
+	});
 });
 
 bot.launch();
